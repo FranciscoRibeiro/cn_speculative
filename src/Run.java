ef89fdb0be5245151c706c1b5071079dd11af0a6
FranciscoRibeiro <kikoribeiro95@gmail.com>
2017-03-31 21:04:19 +0100

    Add coded rules

--- src/Run.java
+++ src/Run.java
@@ -131,21 +131,233 @@ public class Run{
     Constraint c5 = createConstraint("D", Constraint.GT, "60");
     Constraint c6 = createConstraint("R", Constraint.GT, "7.5");
     List<Constraint> constraints2 = new ArrayList<>();
-    constraints1.add(c3);
-    constraints1.add(c4);
-    constraints1.add(c5);
-    constraints1.add(c6);
+    constraints2.add(c3);
+    constraints2.add(c4);
+    constraints2.add(c5);
+    constraints2.add(c6);
     AskableAtom aa2 = createAskableAtom("genre", "G", "db");
     AskableAtom aa3 = createAskableAtom("duration", "D", "db");
     AskableAtom aa4 = createAskableAtom("rating", "R", "db");
     Atom a2 = createSingleAtom("exists", "F");
     List<Atom> body2 = new ArrayList<>();
-    body1.add(aa2);
-    body1.add(aa3);
-    body1.add(aa4);
-    body1.add(a2);
+    body2.add(aa2);
+    body2.add(aa3);
+    body2.add(aa4);
+    body2.add(a2);
     Rule rule2 = new Rule(head2, constraints2, body2);
 
+    /* recommended(F, A) ← A ∈ {yes}, L ∈ {no}, M ∈ {yes}||tooLong(F, L), masterpiece(F, M). */
+    Atom head3 = createAtom("recommended", "A");
+    Constraint c7 = createConstraint("A", Constraint.IN, "yes");
+    Constraint c8 = createConstraint("L", Constraint.IN, "no");
+    Constraint c9 = createConstraint("M", Constraint.IN, "yes");
+    List<Constraint> constraints3 = new ArrayList<>();
+    constraints3.add(c7);
+    constraints3.add(c8);
+    constraints3.add(c9);
+    Atom a3 = createAtom("tooLong", "L");
+    Atom a4 = createAtom("masterpiece", "M");
+    List<Atom> body3 = new ArrayList<>();
+    body3.add(a3);
+    body3.add(a4);
+    Rule rule3 = new Rule(head3, constraints3, body3);
+
+    /* recommended(F, A) ← A ∈ {yes}, T ∈ {yes}||trendingMovie(F, T). */
+    Atom head4 = createAtom("recommended", "A");
+    Constraint c10 = createConstraint("A", Constraint.IN, "yes");
+    Constraint c11 = createConstraint("T", Constraint.IN, "yes");
+    List<Constraint> constraints4 = new ArrayList<>();
+    constraints4.add(c10);
+    constraints4.add(c11);
+    Atom a5 = createAtom("trendingMovie", "T");
+    List<Atom> body4 = new ArrayList<>();
+    body4.add(a5);
+    Rule rule4 = new Rule(head4, constraints4, body4);
+
+    /* recommended(F, A) ← A ∈ {no}, L ∈ {yes}||tooLong(F, L)@db. */
+    Atom head5 = createAtom("recommended", "A");
+    Constraint c12 = createConstraint("A", Constraint.IN, "no");
+    Constraint c13 = createConstraint("L", Constraint.IN, "yes");
+    List<Constraint> constraints5 = new ArrayList<>();
+    constraints5.add(c12);
+    constraints5.add(c13);
+    Atom a6 = createAtom("tooLong", "L");
+    List<Atom> body5 = new ArrayList<>();
+    body5.add(a6);
+    Rule rule5 = new Rule(head5, constraints5, body5);
+
+    /* recommended(F, A) ← A ∈ {no}, T G ∈ {no}, M ∈ {no}||topGenre(F, T G), masterpiece(F, M). */
+    Atom head6 = createAtom("recommended", "A");
+    Constraint c14 = createConstraint("A", Constraint.IN, "no");
+    Constraint c15 = createConstraint("TG", Constraint.IN, "no");
+    Constraint c16 = createConstraint("M", Constraint.IN, "no");
+    List<Constraint> constraints6 = new ArrayList<>();
+    constraints6.add(c14);
+    constraints6.add(c15);
+    constraints6.add(c16);
+    Atom a7 = createAtom("topGenre", "TG");
+    Atom a8 = createAtom("masterpiece", "M");
+    List<Atom> body6 = new ArrayList<>();
+    body6.add(a7);
+    body6.add(a8);
+    Rule rule6 = new Rule(head6, constraints6, body6);
+
+    /* tooLong(F, A) ← A ∈ {yes}, D > 240||duration(F, D)@db, exists(F). */
+    Atom head7 = createAtom("tooLong", "A");
+    Constraint c17 = createConstraint("A", Constraint.IN, "yes");
+    Constraint c18 = createConstraint("D", Constraint.GT, "240");
+    List<Constraint> constraints7 = new ArrayList<>();
+    constraints7.add(c17);
+    constraints7.add(c18);
+    AskableAtom aa5 = createAskableAtom("duration", "D","db" );
+    Atom a9 = createSingleAtom("exists", "F");
+    List<Atom> body7 = new ArrayList<>();
+    body7.add(aa5);
+    body7.add(a9);
+    Rule rule7 = new Rule(head7, constraints7, body7);
+
+    /* tooLong(F, A) ← A ∈ {no}, D ≤ 240||duration(F, D)@db, exists(F). */
+    Atom head8 = createAtom("tooLong", "A");
+    Constraint c19 = createConstraint("A", Constraint.IN, "no");
+    Constraint c20 = createConstraint("D", Constraint.LEQ, "240");
+    List<Constraint> constraints8 = new ArrayList<>();
+    constraints8.add(c19);
+    constraints8.add(c20);
+    AskableAtom aa6 = createAskableAtom("duration", "D","db" );
+    Atom a10 = createSingleAtom("exists", "F");
+    List<Atom> body8 = new ArrayList<>();
+    body8.add(aa6);
+    body8.add(a10);
+    Rule rule8 = new Rule(head8, constraints8, body8);
+
+    /* masterpiece(F, A) ← A ∈ {yes}, R ≥ 8.3, P < 3||rating(F, R)@db, popularity(F, P)@db, exists(F). */
+    Atom head9 = createAtom("masterpiece", "A");
+    Constraint c21 = createConstraint("A", Constraint.IN, "yes");
+    Constraint c22 = createConstraint("R", Constraint.GEQ, "8.3");
+    Constraint c23 = createConstraint("P", Constraint.LT, "3");
+    List<Constraint> constraints9 = new ArrayList<>();
+    constraints9.add(c21);
+    constraints9.add(c22);
+    constraints9.add(c23);
+    AskableAtom aa7 = createAskableAtom("rating", "R","db" );
+    AskableAtom aa8 = createAskableAtom("popularity", "P","db" );
+    Atom a11 = createSingleAtom("exists", "F");
+    List<Atom> body9 = new ArrayList<>();
+    body9.add(aa7);
+    body9.add(aa8);
+    body9.add(a11);
+    Rule rule9 = new Rule(head9, constraints9, body9);
+
+    /* masterpiece(F, A) ← A ∈ {no}, R < 8.3||rating(F, R)@db, exists(F). */
+    Atom head10 = createAtom("masterpiece", "A");
+    Constraint c24 = createConstraint("A", Constraint.IN, "no");
+    Constraint c25 = createConstraint("R", Constraint.LT, "8.3");
+    List<Constraint> constraints10 = new ArrayList<>();
+    constraints10.add(c24);
+    constraints10.add(c25);
+    AskableAtom aa9 = createAskableAtom("rating", "R","db" );
+    Atom a12 = createSingleAtom("exists", "F");
+    List<Atom> body10 = new ArrayList<>();
+    body10.add(aa9);
+    body10.add(a12);
+    Rule rule10 = new Rule(head10, constraints10, body10);
+
+    /* masterpiece(F, A) ← A ∈ {no}, P ≥ 3||popularity(F, P)@db, exists(F). */
+    Atom head11 = createAtom("masterpiece", "A");
+    Constraint c26 = createConstraint("A", Constraint.IN, "no");
+    Constraint c27 = createConstraint("P", Constraint.GEQ, "8.3");
+    List<Constraint> constraints11 = new ArrayList<>();
+    constraints11.add(c26);
+    constraints11.add(c27);
+    AskableAtom aa10 = createAskableAtom("popularity", "P","db" );
+    Atom a13 = createSingleAtom("exists", "F");
+    List<Atom> body11 = new ArrayList<>();
+    body11.add(aa10);
+    body11.add(a13);
+    Rule rule11 = new Rule(head11, constraints11, body11);
+
+    /* topGenre(F, A) ← A ∈ {no}, G /∈ {sci−f i, thriller}||genre(F, G)@db, exists(F). */
+    Atom head12 = createAtom("topGenre", "A");
+    Constraint c28 = createConstraint("A", Constraint.IN, "no");
+    Constraint c29 = createConstraint("G", Constraint.IN, "sci-fi");
+    Constraint c30 = createConstraint("G", Constraint.IN, "thriller");
+    List<Constraint> constraints12 = new ArrayList<>();
+    constraints12.add(c28);
+    constraints12.add(c29);
+    constraints12.add(c30);
+    AskableAtom aa11 = createAskableAtom("genre", "G","db" );
+    Atom a14 = createSingleAtom("exists", "F");
+    List<Atom> body12 = new ArrayList<>();
+    body12.add(aa11);
+    body12.add(a14);
+    Rule rule12 = new Rule(head12, constraints12, body12);
+
+    /* trendingMovie(F, A) ← A ∈ {yes}, P ≤ 2||popularity(F, P)@db, exists(F). */
+    Atom head13 = createAtom("trendingMovie", "A");
+    Constraint c31 = createConstraint("A", Constraint.IN, "yes");
+    Constraint c32 = createConstraint("P", Constraint.LEQ, "2");
+    List<Constraint> constraints13 = new ArrayList<>();
+    constraints13.add(c31);
+    constraints13.add(c32);
+    AskableAtom aa12 = createAskableAtom("popularity", "P","db" );
+    Atom a15 = createSingleAtom("exists", "F");
+    List<Atom> body13 = new ArrayList<>();
+    body13.add(aa12);
+    body13.add(a15);
+    Rule rule13 = new Rule(head13, constraints13, body13);
+
+    /* exists(F) ← F ∈ {TheMatrix}||. */
+    Atom head14 = createSingleAtom("exists", "F");
+    Constraint c33 = createConstraint("F", Constraint.IN, "TheMatrix");
+    List<Constraint> constraints14 = new ArrayList<>();
+    constraints14.add(c33);
+    Rule rule14 = new Rule(head14, constraints14, null);
+
+    /* exists(F) ← F ∈ {Inception}||. */
+    Atom head15 = createSingleAtom("exists", "F");
+    Constraint c34 = createConstraint("F", Constraint.IN, "Inception");
+    List<Constraint> constraints15 = new ArrayList<>();
+    constraints15.add(c34);
+    Rule rule15 = new Rule(head15, constraints15, null);
+
+    /* exists(F) ← F ∈ {Memento}||. */
+    Atom head16 = createSingleAtom("exists", "F");
+    Constraint c35 = createConstraint("F", Constraint.IN, "Memento");
+    List<Constraint> constraints16 = new ArrayList<>();
+    constraints16.add(c35);
+    Rule rule16 = new Rule(head16, constraints16, null);
+
+    /* exists(F) ← F ∈ {ThePrestige}||. */
+    Atom head17 = createSingleAtom("exists", "F");
+    Constraint c36 = createConstraint("F", Constraint.IN, "ThePrestige");
+    List<Constraint> constraints17 = new ArrayList<>();
+    constraints17.add(c36);
+    Rule rule17 = new Rule(head17, constraints17, null);
+
+    /* exists(F) ← F ∈ {TheLorfOfTheRings}||. */
+    Atom head18 = createSingleAtom("exists", "F");
+    Constraint c37 = createConstraint("F", Constraint.IN, "TheLorfOfTheRings");
+    List<Constraint> constraints18 = new ArrayList<>();
+    constraints18.add(c37);
+    Rule rule18 = new Rule(head18, constraints18, null);
+
+    /* exists(F) ← F ∈ {Arrival}||. */
+    Atom head19 = createSingleAtom("exists", "F");
+    Constraint c38 = createConstraint("F", Constraint.IN, "Arrival");
+    List<Constraint> constraints19 = new ArrayList<>();
+    constraints19.add(c38);
+    Rule rule19 = new Rule(head19, constraints19, null);
+
+    List<Rule> rules = new ArrayList<>();
+    rules.add(rule1); rules.add(rule2); rules.add(rule3);
+    rules.add(rule4); rules.add(rule5); rules.add(rule6);
+    rules.add(rule7); rules.add(rule8); rules.add(rule9);
+    rules.add(rule10); rules.add(rule11); rules.add(rule12);
+    rules.add(rule13); rules.add(rule14); rules.add(rule15);
+    rules.add(rule16); rules.add(rule17); rules.add(rule18);
+    rules.add(rule19);
+
+    return rules;
   }
 
   public static void main(String[] args){
